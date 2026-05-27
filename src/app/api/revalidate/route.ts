b586baa8955b622353d206493df821ac849d4090
智絵理 石﨑 <chieri.ishizaki.54@S-MAC-152noMacBook-Pro-3.local>
2026-05-27 16:52:29 +0900

    Add API route to manually revalidate Next.js cache

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,17 @@
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get('path');
+  
+  if (path) {
+    revalidatePath(path);
+    return NextResponse.json({ revalidated: true, path, now: Date.now() });
+  }
+  
+  return NextResponse.json({
+    revalidated: false,
+    message: 'Missing path to revalidate',
+    now: Date.now(),
+  });
+}
